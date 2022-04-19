4f34568dc8272aeddc404a4850d787dbabf8eb92
Tyler Zon <tylerzon@yahoo.com>
2022-04-18 23:29:27 -0400

    Latest changes

--- RHCP_Scrape.py
+++ RHCP_Scrape.py
@@ -5,7 +5,11 @@ from itertools import groupby
 def main():
 
     Under_the_bridge = scrape("https://www.azlyrics.com/lyrics/redhotchilipeppers/underthebridge.html")
-
+    print(Under_the_bridge)
+    californication = scrape("https://www.azlyrics.com/lyrics/redhotchilipeppers/californication.html")
+    print(californication)
+    easily = scrape("https://www.azlyrics.com/lyrics/redhotchilipeppers/easily.html")
+    print(easily)
 
 def scrape(URL):
     page = requests.get(URL)
@@ -16,7 +20,15 @@ def scrape(URL):
     strings = text.split(" ")
     strings_2 = text.split("\n")
 
-    down = ["a", "b", "c", "d", "b", "e", "r"]
+    link_database = []
+    for link in soup.find_all('a', href=True):
+        print(link['href'])
+        if link['href'] not in link_database:
+            link_database.append(link['href'])
+    
+    print(link_database)
+
+
     poslist = []
     i=0
     for pos, item in enumerate(strings_2):
@@ -30,7 +42,7 @@ def scrape(URL):
             answerlist.append(strings_2[poslist[i-1]+1:poslist[i]+1])
     answerlist.append(strings_2[poslist[i]+1:])
     str_list = list(filter(None, answerlist[0]))
-    return str_list
+    return str_list[31:]
 
 #i = (list(g) for _, g in groupby(strings_2, key='\n'.__ne__))
 #print([a + b for a, b in zip(i, i)])
